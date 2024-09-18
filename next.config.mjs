d6fa01034eabfb295b8e814f8a53a88d20ef3563
Evan Sizemore <evan@wool.homes>
2024-09-17 21:37:26 -0400

    linting with type safe env

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import createJITI from 'jiti';
+import { fileURLToPath } from 'node:url';
+const jiti = createJITI(fileURLToPath(import.meta.url));
+
+jiti('./src/env.ts')
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,22 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { ZodError, z } from "zod";
 
 export const env = createEnv({
   // Declare our server-side environment variables
   server: {
-    DATABASE_URL: z.string().url(),
-    OPEN_AI_API_KEY: z.string().min(1),
+    NODE_ENV: z.enum(["development", "test", "production"]),
   },
 
   // Declare our server-side environment variables
-  client: {
-    NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
-  },
+  client: {},
 
   // Declare our client-side environment variables from process.env
-  experimental__runtimeEnv: {
-    NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  experimental__runtimeEnv: {},
+  emptyStringAsUndefined: true,
+  // Called when the schema validation fails.
+  onValidationError: (error: ZodError) => {
+    console.error("\n❌ Invalid environment variables:");
+    console.error(error.flatten().fieldErrors, "\n\n\n");
+    process.exit(1);
   },
 });
