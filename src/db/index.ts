6c9b40d9db2c2a008888832406d51265416e241e
Evan Sizemore <evan@wool.homes>
2024-09-17 22:14:41 -0400

    database url set via env variable

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db");
+import { env } from "@/env";
+
+const queryClient = postgres(env.DATABASE_URL);
 export const db = drizzle(queryClient);
